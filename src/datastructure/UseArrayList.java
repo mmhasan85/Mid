e1df20a0c021d83785a1e2f604793377756416de
hasan <mehedi_ag@yahoo.com>
2018-09-13 00:26:27 -0400

    Map is edited

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -9,59 +9,61 @@ import java.util.List;
 
 public class UseArrayList {
 
-	public static void main(String[] args) {
-		/*
-		 * Demonstrate how to use ArrayList that includes add,peek,remove,retrieve elements.
-		 * Use For Each loop and while loop with Iterator to retrieve data.
-		 * Store all the sorted data into one of the databases.
-		 * 
-		 */
+    public static void main(String[] args) {
+        /*
+         * Demonstrate how to use ArrayList that includes add,peek,remove,retrieve elements.
+         * Use For Each loop and while loop with Iterator to retrieve data.
+         * Store all the sorted data into one of the databases.
+         *
+         */
 
-		ArrayList<Integer>arrli=new ArrayList();
+        ArrayList<Integer> arrli = new ArrayList();
 //add element to ArrayList
-		arrli.add(23);
-		arrli.add(111);
-		arrli.add(120);
-		arrli.add(260);
-		arrli.add(295);
+        arrli.add(23);
+        arrli.add(111);
+        arrli.add(120);
+        arrli.add(260);
+        arrli.add(295);
 //print ArrayList
-		System.out.println("Retrieving elements from ArrayList: "+arrli);
+        System.out.println("Retrieving elements from ArrayList: " + arrli);
 //Using get method to peek the 1st element
-		System.out.println("Peek first element: "+arrli.get(0));
+        System.out.println("Peek first element: " + arrli.get(0));
 //remove element from ArrayList
-		System.out.println("Remove element at index 1: "+ arrli.remove(1));
+        System.out.println("Remove element at index 1: " + arrli.remove(1));
 //Retrieving elements from ArrayList
-		System.out.println("Re-retrieving elements from ArrayList: "+arrli);
+        System.out.println("Re-retrieving elements from ArrayList: " + arrli);
 //Using 'for-each'loop
-		System.out.println("Use of for-each loop: ");
-		for(Integer in :arrli){
-			System.out.println(in);
-		}
-		//Using iterator
-		System.out.println("Use of iterator using while loop: ");
-		Iterator it = arrli.iterator();
-		while(it.hasNext()) {
-			Integer i =(Integer)it.next();
-			System.out.println(i);
+        System.out.println("Use of for-each loop: ");
+        for (Integer in : arrli) {
+            System.out.println(in);
+        }
+        //Using iterator
+        System.out.println("Use of iterator using while loop: ");
+        Iterator it = arrli.iterator();
+        while (it.hasNext()) {
+            Integer i = (Integer) it.next();
+            System.out.println(i);
 
-		}
+        }
 //Sorting ArrayList
-		Collections.sort(arrli);
-		System.out.println(arrli);
-		List list = new ArrayList();
-		list =arrli;
-		//connect to MYSql Database
-		ConnectToSqlDB connectDB=new ConnectToSqlDB();
-		//Create table in the database
-		connectDB.createTableFromToMySql("emp_record", "use_map", "arr_li");
-		//insert data in the database
-		connectDB.insertDataFromArrayListToSqlTable(list,"arr_li","listElement");
-		//Read data from database
-		//List<String> numbers= (List<String>) connectDB.readDataBase("arr_list","listElement");
-		//for(String st:numbers){
-		//	System.out.println(st);
-		}
-	}
+        Collections.sort(arrli);
+        System.out.println(arrli);
+        List list = new ArrayList();
+        list = arrli;
+        //connect to MYSql Database
+        ConnectToSqlDB connectDB = new ConnectToSqlDB();
+        //Create table in the database
+        connectDB.createTableFromToMySql("emp_record", "use_map", "arr_li");
+        //insert data in the database
+        connectDB.insertDataFromArrayListToSqlTable(list, "arr_li", "listElement");
+        //Read data from database
+        //
+        /*List<String> numbers = (List<String>) connectDB.readDataBase("arr_list", "listElement");
+        for (String st : numbers) {
+            System.out.println(st);
+        }*/
+    }
+}
 
 
 
--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.*;
+
 public class UseMap {
 
 	public static void main(String[] args) {
@@ -10,6 +12,63 @@ public class UseMap {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
+
+		Map<String, Integer> hm = new HashMap<String, Integer>();
+		hm.put("chicken", new Integer(5));
+		hm.put("beef", new Integer(9));
+		hm.put("goat", new Integer(12));
+		hm.put("salmon", new Integer(10));
+		Set<Map.Entry<String, Integer>> st = hm.entrySet();
+		for (Map.Entry<String, Integer> me : st) {
+			System.out.print(me.getKey() + " : ");
+			System.out.println(me.getValue() + "lb");
+		}
+		System.out.println("+++++++++++++++++++++++++++++++++++++++");
+
+		HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+
+		List<String> fruitslist = new ArrayList<String>();
+		fruitslist.add("watermelon");
+		fruitslist.add("kiwi");
+		fruitslist.add("orange");
+		map.put("fruit", (ArrayList<String>) fruitslist);
+		System.out.println(fruitslist);
+		System.out.println("++++++++++++++++++++++++++++++++++++++");
+
+		ArrayList<String> carlist = new ArrayList<String>();
+		carlist.add("toyota");
+		carlist.add("bmw");
+		carlist.add("audi");
+		map.put("car", carlist);
+		System.out.println(map);
+		System.out.println("===================================");
+
+		for (Object str : map.keySet()) {
+			System.out.println("keyset:" + str);
+			for (String str1 : map.get(str)) {
+				System.out.println("Value: " + str1);
+			}
+		}
+		System.out.println("====================================");
+
+		Map<String, List<String>> data = new HashMap<String, List<String>>();
+		data.put("Computer language", Arrays.asList("Java", "Python", "C++", "C#", "Visual", "Basic.NET"));
+		data.put("Lunch", Arrays.asList("dumplings", "hotpot", "cold dishes", "pizza"));
+		data.put("Tea", Arrays.asList("Olong tea", "Jasmin tea", "Green tea"));
+		System.out.println("Printing Hashmap using For each loop");
+		//Hashmap-for each loop
+		for (Map.Entry<String, List<String>> printdata : data.entrySet()) {
+			System.out.println(printdata.getKey() + "--->" + printdata.getValue());
+		}
+		System.out.println("+++++++++++++++++++++++++++++++++++++");
+		System.out.println("Printing Hashmap using Iterator");
+		//Hashmap-Iterator
+		Iterator<Map.Entry<String, List<String>>> it = data.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<String, List<String>> group = it.next();
+			System.out.println(group.getKey() + "--->" + group.getValue());
+		}
+
 
 	}
 
